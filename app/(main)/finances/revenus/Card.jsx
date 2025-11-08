41823b1ea41c0b49f375fbd4a5e5c7b847747a8b
bouabre225 <158521003+bouabre225@users.noreply.github.com>
2025-11-08 12:46:10 +0100

    modification du style

--- app/(main)/finances/revenus/Card.jsx
+++ app/(main)/finances/revenus/Card.jsx
@@ -7,43 +7,51 @@ export default function Card ({ title, value, trend, icon: Icon, bgColor, bgColo
 
     return (
         <div className={cn("p-4 rounded-xl", bgColor)}>
-            <div className="flex items-start justify-between mb-2">                
-                <div className={cn("w-8 h-8 bg-white/30 rounded flex items-center justify-center",bgColorIcon)}>
+            <div className="flex items-center gap-2 mb-2">                
+                <div className={cn("w-8 h-8 bg-[#FFFFFF] rounded flex items-center justify-center",bgColorIcon)}>
                     {Icon && <Icon className={"w-4 h-4 text-black"} />}
                 </div>
-                <span className="text-sm text-black text-foreground/70">{title}</span>
-
-                <div className="flex items-start">
-                    <button className="text-muted-foreground hover:text-foreground">
-                        <MoreHorizontal className="w-5 h-5" />
-                    </button>
-                </div>
+                <span className="text-xs text-[#6E6E6E]">{title}</span>             
+            </div>
+            <div className="flex items-center justify-end]">
+                <button className="text-muted-foreground hover:text-foreground">
+                    <MoreHorizontal color="#6E6E6E" className="w-5 h-5" />
+                </button>
             </div>
 
-            <div className="text-2xl font-bold mb-1.5">{value}</div>
+            <div className="text-2xl items-end flex font-bold gap-5 text-[#0D0E0D]">
+                {value}
+            </div>
 
-            {trend !== undefined && (
-                <div className="flex items-center gap-1 text-xs">
-                    <div className={cn(bgColorTrend, "grid grid-cols-2 items-center rounded-2xl gap-1 justify-between")}>
-                        {isPositive && <TrendingUp className="w-3 h-3 text-black" />}
-                        {isNegative && <TrendingDown className="w-3 h-3 text-black" />}
-                        <span
-                            className={cn(
-                                "font-medium flex",
-                                isPositive && "text-black",
-                                isNegative && "text-black",
+            <div className="flex justify-end text-xs font-bold mb-1.5 mr-3 ">
+                {trend !== undefined && (
+                    <div className="flex items-center gap-1 text-xs">
+                        <div className={cn(bgColorTrend, "flex gap-2 items-center rounded-lg")}>
+                                {isPositive && <TrendingUp className="w-4 h-4" />}
+                                {isNegative && <TrendingDown className="w-4 h-4" />}
+                                <span
+                                    className={cn(
+                                        "font-medium flex",
+                                        isPositive && "text-[#0D0E0D]",
+                                        isNegative && "text-[#0D0E0D]",
 
-                            )}
-                        >{" "}
-                        {isPositive ? "" : ""}
-                            {trend}%
-                    </span>
+                                    )}
+                                >{" "}
+                                {isPositive ? "" : ""}
+                                    {trend}%
+                            </span>
+                            
+                        </div>
+                        
+
                     </div>
-                    <span className="text-gray-150 text-sm flex justify-end">
-                        from last week {" "}
-                    </span>
-                </div>
-            )}
+                )}
+            </div>
+            <div className="flex justify-end ">
+                <span className="text-[#A3A3A3] text-xs flex justify-end">
+                    from last week {" "}
+                </span>
+            </div>
         </div>
     );
 };
