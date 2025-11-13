89cfbfd795b8e9a6f8a0b04697f316a59f75ffed
bouabre225 <158521003+bouabre225@users.noreply.github.com>
2025-11-13 12:39:23 +0100

    mise en place de la page de profil

---
+++ app/(main)/reservations/valide/PageProfil.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState } from "react";
+import { Search, CalendarDays, Plus, Eye, Edit , ChevronUp, ChevronDown, ChevronRight, Filter, Phone, MailOpen} from "lucide-react";
+import {ChevronUpDownIcon, FunnelIcon } from "@heroicons/react/24/solid";
+
+
+export default function PageProfil () {
+    return (
+        <div>
+            <h1>PageProfil</h1>
+        </div>
+    )
+}
