a4d640ded688047e376c9dd62718b3cfde9a68a4
ABOKAJR <marie.edmond.dossa@gmail.com>
2025-11-29 19:50:52 +0100

    refactor: remove unused authentication pages and redirect logic

--- app/page.js
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { redirect } from "next/navigation";
-
-export default function Home() {
-  // Redirect visitors hitting the root to the login screen.
-  redirect("/login");
-}
